0145493d0b505da4cc6bda3964fdda53cefe9e6d
cgomesu <cf365@cornell.edu>
2022-03-26 08:51:13 -0300

    Add Pandera integration and minor readability changes

--- bolsas.py
+++ bolsas.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#########################################################
+################################################################
 # Programa desenvolvido para processo de seleção
 #
 # Autor:  Carlos Gomes (cgomesu)
@@ -12,7 +12,8 @@
 # Documentação relacionada:
 # - curses: https://docs.python.org/3/library/curses.html
 # - pandas: https://pandas.pydata.org/docs/
-#########################################################
+# - pandera: https://pandera.readthedocs.io/en/stable/index.html
+################################################################
 
 import curses
 from curses.textpad import Textbox
@@ -31,10 +32,15 @@ MENU = [
 ## diretório de trabalho dos dados e nome do arquivo com dados
 DIR_TRABALHO = "data/"
 ARQUIVO_CSV = "br-capes-bolsistas-uab.csv"
+## linha inicial do menu
+LINHA_INICIAL = 0
 ## número máximo de caracteres no input de texto
 TEXTBOX_CARACTERES_MAX = 60
 ## ranking das maiores e menores bolsas
 RANKING_BOLSAS = 3
+## validação de dados
+LOG_DIR = "logs/"
+LOG_FAILURE_CASES = "failure_cases.log"
 
 def imprimir_menu_centro(janela, linha_selecionada):
     janela.clear()
@@ -105,8 +111,7 @@ def opcao_consulta_bolsa_ano(janela, dados):
             "Nome: '{}'".format(dados_ano.iloc[0]["NM_BOLSISTA"]),
             "CPF: '{}'".format(dados_ano.iloc[0]["CPF_BOLSISTA"]),
             "Entidade de ensino: '{}'".format(dados_ano.iloc[0]["NM_ENTIDADE_ENSINO"]),
-            "Valor da bolsa (em {}): '{}'".format(dados_ano.iloc[0]["CD_MOEDA"],dados_ano.iloc[0]["VL_BOLSISTA_PAGAMENTO"]),]
-            )
+            "Valor da bolsa (em {}): '{}'".format(dados_ano.iloc[0]["CD_MOEDA"],dados_ano.iloc[0]["VL_BOLSISTA_PAGAMENTO"]),])
 
 def opcao_procura_codifica_nome(janela, dados):
     nome_digitado = utils.formatador(input_textbox_string_centro(janela, "Digite o NOME abaixo:"))
@@ -116,13 +121,10 @@ def opcao_procura_codifica_nome(janela, dados):
         ## loop casos com nome para perguntar ao usuario
         for i in range(len(dados_com_nome[["NM_BOLSISTA"]])):
             if re.match("^.*"+nome_digitado+".*$", dados_com_nome.iloc[i-1]["NM_BOLSISTA"]):
-                imprimir_lista_centro(janela, lista=[
-                    "Nome encontrado:",
-                    "'{}'".format(dados_com_nome.iloc[i-1]["NM_BOLSISTA"]),
-                    "",
+                imprimir_lista_centro(janela, lista=["Nome encontrado:",
+                    "'{}'".format(dados_com_nome.iloc[i-1]["NM_BOLSISTA"]), "",
                     "Pressione 'ENTER' para CONFIRMAR",
-                    "ou pressione outra telca para ir ao próximo.",]
-                    )
+                    "ou pressione outra telca para ir ao próximo.",])
                 if janela.getch() == 10:
                     ## usar utilitario para codificar nome via join (com espaco) + list comprehension
                     nome_codificado = ' '.join(utils.codificador(n) for n in dados_com_nome.iloc[i-1]["NM_BOLSISTA"].split())
@@ -130,8 +132,7 @@ def opcao_procura_codifica_nome(janela, dados):
                         "Nome (codificado): '{}'".format(nome_codificado),
                         "Ano: '{}'".format(dados_com_nome.iloc[i-1]["AN_REFERENCIA"]),
                         "Entidade de ensino: '{}'".format(dados_com_nome.iloc[i-1]["NM_ENTIDADE_ENSINO"]),
-                        "Valor da bolsa (em {}): '{}'".format(dados_com_nome.iloc[i-1]["CD_MOEDA"],dados_com_nome.iloc[i-1]["VL_BOLSISTA_PAGAMENTO"]),]
-                        )
+                        "Valor da bolsa (em {}): '{}'".format(dados_com_nome.iloc[i-1]["CD_MOEDA"],dados_com_nome.iloc[i-1]["VL_BOLSISTA_PAGAMENTO"]),])
                     break
     else:
         imprimir_string_centro(janela, string="Não encontrado bolsista '{}'".format(nome_digitado))
@@ -152,60 +153,62 @@ def opcao_consulta_media_anual(janela, dados):
         imprimir_string_centro(janela, string="Ano {} não encontrado.".format(ano_digitado))
     else:
         imprimir_lista_centro(janela, lista=["Média do valor das bolas do Ano {}".format(ano_digitado),
-            "média = '{:.2f}'".format(dados_ano["VL_BOLSISTA_PAGAMENTO"].mean())]
-            )
+            "média = '{:.2f}'".format(dados_ano["VL_BOLSISTA_PAGAMENTO"].mean())])
 
 def opcao_ranking_bolsas(janela, dados, ranking=RANKING_BOLSAS):
-    ## use de nlargest e nsmallest para achar ranking bolsas
+    ## use nlargest e nsmallest para achar ranking bolsas
     dados_tres_maiores_bolsas = dados.nlargest(ranking, "VL_BOLSISTA_PAGAMENTO")
     dados_tres_menores_bolsas = dados.nsmallest(ranking, "VL_BOLSISTA_PAGAMENTO")
     for i in range(ranking):
         imprimir_lista_centro(janela, lista=["Os {} alunos com bolsa mais ALTA:".format(ranking),
-        "{}. '{}' ({} {})".format(i+1,
-            dados_tres_maiores_bolsas.iloc[i]["NM_BOLSISTA"],
-            dados_tres_maiores_bolsas.iloc[i]["CD_MOEDA"],
-            dados_tres_maiores_bolsas.iloc[i]["VL_BOLSISTA_PAGAMENTO"]),
-            "", "Pressione qualquer tecla para ir ao próximo..."]
-            )
+            "{}. '{}' ({} {})".format(i+1,
+                dados_tres_maiores_bolsas.iloc[i]["NM_BOLSISTA"],
+                dados_tres_maiores_bolsas.iloc[i]["CD_MOEDA"],
+                dados_tres_maiores_bolsas.iloc[i]["VL_BOLSISTA_PAGAMENTO"]),
+            "", "Pressione qualquer tecla para ir ao próximo..."])
         janela.getch()
     for i in range(ranking):
         imprimir_lista_centro(janela, lista=["Os {} alunos com bolsa mais BAIXA:".format(ranking),
-        "{}. '{}' ({} {})".format(i+1,
-            dados_tres_menores_bolsas.iloc[i]["NM_BOLSISTA"],
-            dados_tres_menores_bolsas.iloc[i]["CD_MOEDA"],
-            dados_tres_menores_bolsas.iloc[i]["VL_BOLSISTA_PAGAMENTO"]),
-            "", "Pressione qualquer tecla para ir ao próximo..."]
-            )
+            "{}. '{}' ({} {})".format(i+1,
+                dados_tres_menores_bolsas.iloc[i]["NM_BOLSISTA"],
+                dados_tres_menores_bolsas.iloc[i]["CD_MOEDA"],
+                dados_tres_menores_bolsas.iloc[i]["VL_BOLSISTA_PAGAMENTO"]),
+            "", "Pressione qualquer tecla para ir ao próximo..."])
         if i < ranking-1:
             janela.getch()
 
-def main(janela):
-    ## inicializar configuração básica do curses
-    curses.curs_set(False)  ##cursor
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)  ##par da cor de seleção padrão
-    linha_atual = 0  ##linha selecionada inicial
-
+def inicializar_dados(janela):
     ## inicializar objeto de dados e criar um df salvo na memoria para manipulação
-    imprimir_lista_centro(janela, lista=["Lendo o arquivo CSV:",
-        "'{}'".format(ARQUIVO_CSV)]
-        )
-    dados,err = utils.Dados(DIR_TRABALHO+ARQUIVO_CSV).csv_to_dataframe()
+    imprimir_lista_centro(janela, lista=["Lendo o arquivo CSV:","'{}'".format(ARQUIVO_CSV)])
+    dados = utils.Dados(DIR_TRABALHO+ARQUIVO_CSV).csv_to_dataframe()
     sleep(1)
-    if err:
-        imprimir_lista_centro(janela, lista=["Atenção! O programa não conseguiu ler o arquivo CSV com opções padrão:",
-            "'{}'".format(ARQUIVO_CSV),
-            "",
-            "O programa continuará, porém é possível que alguns dados foram perdidos no processo.",
-            "Pressione qualquer tecla para continuar..."]
-            )
-        janela.getch()
     if dados.empty:
         imprimir_lista_centro(janela, lista=["Erro crítico! Nenhum dado foi lido.",
-            "Verifique o arquivo CSV e tente novamente.",
-            "Pressione qualquer tecla para terminar o programa."]
-            )
+            "Verifique o arquivo CSV e tente novamente.", "",
+            "Pressione qualquer tecla para terminar o programa."])
         janela.getch()
         exit()
+    ## validar dataframe importado para memória
+    imprimir_string_centro(janela, string="Validando os dados...")
+    sleep(1)
+    if not utils.Dados.validate_df(dados, log_failures=LOG_DIR+LOG_FAILURE_CASES):
+        imprimir_lista_centro(janela, lista=["Erro crítico! O dataframe não passou a validação.",
+            "Um log dos erros e casos será gravado em '{}'".format(LOG_DIR+LOG_FAILURE_CASES), "",
+            "Corrigir os problemas no arquivo CSV e tentar novamente.",
+            "Pressione qualquer tecla para terminar o programa."])
+        janela.getch()
+        exit()
+    imprimir_string_centro(janela, string="Dados validados com sucesso!")
+    sleep(2)
+    return dados
+
+def main(janela):
+    ## inicializar configuração básica do curses
+    curses.curs_set(False)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    linha_atual = LINHA_INICIAL
+
+    dados = inicializar_dados(janela)
 
     ## menu principal
     imprimir_menu_centro(janela, linha_atual)
--- utils/dados.py
+++ utils/dados.py
@@ -1,5 +1,20 @@
-import re
 import pandas
+import pandera
+
+## esquema de validação da df via pandera
+SCHEMA_DICT = {
+    "NM_BOLSISTA": pandera.Column(str),
+    "CPF_BOLSISTA": pandera.Column(str),
+    "NM_ENTIDADE_ENSINO": pandera.Column(str),
+    "ME_REFERENCIA": pandera.Column(int),
+    "AN_REFERENCIA": pandera.Column(int),
+    "SG_DIRETORIA": pandera.Column(str),
+    "SG_SISTEMA_ORIGEM": pandera.Column(str),
+    "CD_MODALIDADE_SGB": pandera.Column(int),
+    "DS_MODALIDADE_PAGAMENTO": pandera.Column(str),
+    "CD_MOEDA": pandera.Column(str),
+    "VL_BOLSISTA_PAGAMENTO": pandera.Column(int),
+}
 
 
 class Dados:
@@ -9,9 +24,18 @@ class Dados:
     def csv_to_dataframe(self):
         ## tentar ler formato padrão e em erro, usar opções mais flexíveis, mas risco de perda de dados
         ## https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html?highlight=read_csv
-        err = False
         try:
-            return pandas.read_csv(self.arquivo, sep=";"), err
+            return pandas.read_csv(self.arquivo, sep=",|;", encoding="ISO-8859-1", engine="python")
         except:
-            err = True
-            return pandas.read_csv(self.arquivo, sep=";", encoding="ISO-8859-1", on_bad_lines="skip"), err
+            return pandas.read_csv(self.arquivo, sep=",|;", encoding="ISO-8859-1", engine="python", on_bad_lines="skip")
+
+    @staticmethod
+    def validate_df(df, log_failures):
+        schema = pandera.DataFrameSchema(SCHEMA_DICT)
+        try:
+            schema(df, lazy=True), object()
+            return True
+        except pandera.errors.SchemaErrors as err:
+            with open(log_failures, 'w') as f:
+                f.write("{}".format(err))
+            return False
